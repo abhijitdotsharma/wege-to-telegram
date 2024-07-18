7b39536751c97919e1e701ddcadfc43c19f5222b
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2024-07-18 16:52:32 +0200

    so my bot through `main.js` can send scrapped listings to the telegram group

---
+++ telegram.js
@@ -0,0 +1,34 @@
+// package imports
+import fetch from 'node-fetch';
+
+/**
+ * 
+ * @param {*} text - The message to send to the telegram group
+ * 
+ * Sends listing data to telegram group via a bot.
+ * Usage example - sendMessageToTelegram('Hello, World!');
+ */
+async function sendMessageToTelegram(text) {
+  const botToken = process.env.TELEGRAM_BOT_TOKEN;
+  const chatID = process.env.TELEGRAM_GROUP_CHAT_ID;
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        chat_id: chatID,
+        text: text,
+      }),
+    });
+
+    const data = await response.json();
+    console.log('Message sent to Telegram:', data);
+  } catch (error) {
+    console.error('Error sending message to Telegram:', error);
+  }
+}
+
+
+export { sendMessageToTelegram };
